3b75cf33b85c7bf13c28f17d6e6d507eaffaba38
Kishspawn <57330562+Kishspawn@users.noreply.github.com>
2021-04-18 04:57:11 -0700

    UI
    Started the main menu, made some furniture.  Deleted the github part.

---
+++ Assets/Scripts/UIMgr.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+public class UIMgr : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+    public void LaunchScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+
+    //Quits out of program
+    public void ExitGame()
+    {
+        Debug.Log("Exit");
+        Application.Quit();
+    }
+}
