7cf88afaa4b5cc181db45a8c8d6f4c957046dc4e
Kishspawn <57330562+Kishspawn@users.noreply.github.com>
2021-04-19 14:17:40 -0700

    UI to LVL1. Put Chits in cage
    Changed it from bottle to chit

--- Assets/Scripts/ChitCounter.cs
+++ Assets/Scripts/ChitCounter.cs
@@ -4,13 +4,19 @@ using UnityEngine;
 
 public class ChitCounter : MonoBehaviour
 {
-    
+    public GameObject capturedVersion;
+    int i = 3;
     private void OnTriggerEnter(Collider other)
     {
         //Change Bottle to Chits. Done for testing purposes.
-        if (other.gameObject.name == "Bottle")
+        if (other.gameObject.tag == "Chit")
         {
+            Debug.Log("AHHH");
             Destroy(other.gameObject);
+          
+                //Instantiate(capturedVersion, new Vector3(7.377F, 0.186f, -5.051f), transform.rotation);
+
+            
             UIMgr.inst.numChit -= 1; 
         }
     }
