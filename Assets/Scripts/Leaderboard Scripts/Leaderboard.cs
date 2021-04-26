ece9cc656730308b5e65aee618875e8a1b75ac6b
Jared-Freeman <j99freeman@gmail.com>
2021-04-25 19:40:58 -0700

    leaderboard scene update
    realtime lighting for now

--- Assets/Scripts/Leaderboard Scripts/Leaderboard.cs
+++ Assets/Scripts/Leaderboard Scripts/Leaderboard.cs
@@ -2,17 +2,26 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+//Idea: A leaderboard is a list of player initials and associated scores.
+//Scores should be reported on level end. The leaderboard will then acquire the score and store it in the list.
 public class Leaderboard : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
+    #region MEMBERS
+    #endregion
+    #region EVENTS
+    public static event System.EventHandler<MonobehaviourEventArgs> RequestAlignToCameraAnglesEvent;
+    #endregion
+    #region EVENT SUBSCRIPTIONS
+    #endregion
+    #region EVENT HANDLERS
+    #endregion
+    #region INIT
+    private void Awake()
     {
-        
     }
+    #endregion
+
+
+
 }
