137846ff86c4adc9014f4e4944b5a238c10b3688
Bryan Dedeurwaerder <23252092+BryanDedeur@users.noreply.github.com>
2021-04-26 12:17:10 -0700

    Updated inventory with UI features

--- Assets/Scripts/Inventory/InventorySystem.cs
+++ Assets/Scripts/Inventory/InventorySystem.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 /* 
  * Author: Bryan Dedeurwaerder
@@ -18,25 +19,110 @@ public class InventorySystem : MonoBehaviour
     private Tool equippedTool;
     private int equippedToolIndex;
 
+    public GameObject toolbarUI;
+    public GameObject toolUI;
+
+    private Dictionary<int, GameObject> toolUIObjects;
+
     private void Start()
     {
         inventory = new Dictionary<int, Tool>();
+        toolUIObjects = new Dictionary<int, GameObject>();
+
+        for (int i = 1; i <= maxSize; i++)
+        {
+            inventory.Add(i, null);
+            toolUIObjects.Add(i, Instantiate(toolUI));
+            toolUIObjects[i].transform.parent = toolbarUI.transform;
+            toolUIObjects[i].SetActive(false);
+        }
+
         foreach (Tool tool in transform.GetComponentsInChildren<Tool>())
         {
             Pickup(tool);
         }
     }
 
+    private void ToggleHighlightToolUI(GameObject slot, bool state)
+    {
+        Transform highlight = slot.transform.Find("Highlight");
+        highlight.gameObject.SetActive(state);
+    }
+
+    private void UpdateToolUI(int id, string toolName)
+    {
+        GameObject toolUI = toolUIObjects[id];
+        toolUI.SetActive(true);
+
+        Transform idUI = toolUI.transform.Find("NumID");
+        Transform nameUI = toolUI.transform.Find("Name");
+
+        Text idText = idUI.GetComponent<Text>();
+        idText.text = id.ToString();
+
+        Text nameText = nameUI.GetComponent<Text>();
+        nameText.text = toolName;
+
+        ToggleHighlightToolUI(toolUIObjects[id], true);
+    }
+
+
+
+    public void UnequipEquipped()
+    {
+        if (equippedTool != null)
+        {
+            equippedTool.OnUnequip();
+            equippedTool.gameObject.SetActive(false);
+            equippedTool = null;
+
+            ToggleHighlightToolUI(toolUIObjects[equippedToolIndex], false);
+
+            equippedToolIndex = -1;
+        }
+    }
+
+    public void Equip(int index)
+    {
+        if (inventory[index] != null)
+        {
+            bool equip = equippedToolIndex != index;
+
+            if (equippedTool != null)
+            {
+                UnequipEquipped();
+            }
+
+            if (equip)
+            {
+                inventory[index].gameObject.SetActive(true);
+                inventory[index].transform.parent = Camera.main.transform; // TODO finding the camera should be improved. What if we have multiple cameras?
+                inventory[index].Equip();
+                equippedTool = inventory[index];
+                equippedToolIndex = index;
+
+                ToggleHighlightToolUI(toolUIObjects[equippedToolIndex], true);
+
+            }
+        } 
+    }
+
     public void Pickup(Tool newTool)
     {
-        for (int i = 1; i < maxSize; i++)
-        {
-            if (!inventory.ContainsKey(i))
+        if (!inventory.ContainsValue(newTool)) {
+            foreach (KeyValuePair<int, Tool> pair in inventory)
             {
-                inventory.Add(i, newTool);
-                newTool.OnPickup(transform.parent.gameObject);
-                Equip(i);
-                break;
+                if (pair.Value == null)
+                {
+                    UpdateToolUI(pair.Key, newTool.name);
+                    inventory[pair.Key] = newTool;
+                    newTool.Pickup(transform.parent.gameObject);
+
+                    int key = pair.Key;
+                    Equip(key);
+
+                    return;
+                }
             }
         }
     }
@@ -45,61 +131,36 @@ public class InventorySystem : MonoBehaviour
     {
         if (equippedTool != null)
         {
+            ToggleHighlightToolUI(toolUIObjects[equippedToolIndex], false);
+
+            toolUIObjects[equippedToolIndex].SetActive(false);
+
             equippedTool.gameObject.SetActive(true);
-            equippedTool.OnDrop();
+            equippedTool.Drop();
             equippedTool.transform.parent = null;
 
-            inventory.Remove(equippedToolIndex);
+            inventory[equippedToolIndex] = null;
+            equippedToolIndex = -1;
 
             equippedTool = null;
         }
-    }
-
-    public void UnequipEquipped()
-    {
-        if (equippedTool)
-        {
-            equippedTool.OnUnequip();
-            equippedTool.gameObject.SetActive(false);
-            equippedTool = null;
-        }
-    }
-
-    public void Equip(int index)
-    {
-        if (inventory.ContainsKey(index))
-        {
-            if (equippedTool != null)
-            {
-                UnequipEquipped();
-            }
-
-            inventory[index].gameObject.SetActive(true);
-            inventory[index].transform.parent = Camera.main.transform; // TODO finding the camera should be improved. What if we have multiple cameras?
-            inventory[index].OnEquip();
-            equippedTool = inventory[index];
-            equippedToolIndex = index;
-        }
-        else
-        {
-            Debug.Log("Trying to access a tool that does not exist");
-        }
-
     }
 
     private void Update()
     {
-        if (Input.GetButtonDown("Fire1"))
+        if (equippedTool != null)
         {
-            if (equippedTool != null)
+            if (Input.GetButtonDown("Fire1"))
             {
                 equippedTool.StartAction();
             }
-        }
 
-        if (Input.GetButtonUp("Fire1"))
-        {
-            if (equippedTool != null)
+            if (Input.GetButton("Fire1"))
+            {
+                equippedTool.ContinueAction();
+            }
+
+            if (Input.GetButtonUp("Fire1"))
             {
                 equippedTool.EndAction();
             }
