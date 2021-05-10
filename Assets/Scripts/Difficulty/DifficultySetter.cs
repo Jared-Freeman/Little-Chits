e79b94f5f56eca74850be00028baef0376d93fb4
Jared-Freeman <j99freeman@gmail.com>
2021-05-09 23:34:35 -0700

    finalizing difficulty settings on main menu
    try clicking the buttons!

---
+++ Assets/Scripts/Difficulty/DifficultySetter.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DifficultySetter : MonoBehaviour
+{
+    private bool flag_debug = true;
+    public AudioSource attached_confirmation_sound_easy;
+    public AudioSource attached_confirmation_sound_medium;
+    public AudioSource attached_confirmation_sound_hard;
+
+    public void SetDifficultyEasy()
+    {
+        if (flag_debug) Debug.Log("easy");
+        attached_confirmation_sound_easy.Play();
+        DifficultyManager.current_difficulty = DifficultyManager.Difficulty.Easy;
+    }
+    public void SetDifficultyMedium()
+    {
+        if (flag_debug) Debug.Log("medium");
+        attached_confirmation_sound_medium.Play();
+        DifficultyManager.current_difficulty = DifficultyManager.Difficulty.Medium;
+    }
+    public void SetDifficultyHard()
+    {
+        if (flag_debug) Debug.Log("hard");
+        attached_confirmation_sound_hard.Play();
+        DifficultyManager.current_difficulty = DifficultyManager.Difficulty.Hard;
+    }
+}
